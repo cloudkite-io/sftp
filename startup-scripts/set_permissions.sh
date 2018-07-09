c9adb24e29ca666c45f2db97c176124214fb2705
Adilet Maratov <adilet.maratov@gmail.com>
2018-07-09 13:12:55 +0600

    let users read and write in their upload dirs

--- startup-scripts/set_permissions.sh
+++ startup-scripts/set_permissions.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-echo "Setting read permissions"
+echo "Setting read & write permissions"
 
 HOME_DIR="/home"
 UPLOAD_DIRS=$(find ${HOME_DIR} -maxdepth 1 -mindepth 1 -type d)
 
 for dir in ${UPLOAD_DIRS}
 do
-    chmod 511 ${dir}/upload
-    echo "Set 511 perms to ${dir}/upload"
+    chmod 711 ${dir}/upload
+    echo "Set 711 perms to ${dir}/upload"
 done
